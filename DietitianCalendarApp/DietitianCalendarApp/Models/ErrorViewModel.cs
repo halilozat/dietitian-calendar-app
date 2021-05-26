4624b7576586646c66e59f07626e5c09ed4bce6c
halilozat <halilozat@hotmail.com>
2021-05-26 14:19:07 +0300

    Create a mvc project

---
+++ DietitianCalendarApp/DietitianCalendarApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DietitianCalendarApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
