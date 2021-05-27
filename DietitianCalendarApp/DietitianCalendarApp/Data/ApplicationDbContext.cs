d34441fe18a4abe34dfdf59927225c0d61156cc6
halilozat <halilozat@hotmail.com>
2021-05-27 11:46:44 +0300

    Creating entities for database
    Creating entities for the database with the code first approach.

--- DietitianCalendarApp/DietitianCalendarApp/Data/ApplicationDbContext.cs
+++ DietitianCalendarApp/DietitianCalendarApp/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using DietitianCalendarApp.Data.Entity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -6,11 +7,14 @@ using System.Text;
 
 namespace DietitianCalendarApp.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<AppUser, AppRole, string>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        public DbSet<Appointment> Appoinments { get; set; }
+
     }
 }
---
+++ DietitianCalendarApp/DietitianCalendarApp/Data/Entity/AppUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DietitianCalendarApp.Data.Entity
+{
+    public class AppUser : IdentityUser
+    {
+
+        public bool IsDietitian { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public List<Appointment> Appoinments { get; set; }
+    }
+}
