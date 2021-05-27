262f650447d0e45fe7f31dfd37261ac028978642
halilozat <halilozat@hotmail.com>
2021-05-27 17:58:49 +0300

    login&logout operations

--- DietitianCalendarApp/DietitianCalendarApp/Controllers/AccountController.cs
+++ DietitianCalendarApp/DietitianCalendarApp/Controllers/AccountController.cs
@@ -12,16 +12,48 @@ namespace DietitianCalendarApp.Controllers
     public class AccountController : Controller
     {
         private UserManager<AppUser> _userManager;
-        private SignInManager<AppUser> _signInUser;
+        private SignInManager<AppUser> _signInManager;
         private RoleManager<AppRole> _roleManager;
 
         //Dependency Injection
-        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInUser, RoleManager<AppRole> roleManager)
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, RoleManager<AppRole> roleManager)
         {
             _userManager = userManager;
-            _signInUser = signInUser;
+            _signInManager = signInManager;
             _roleManager = roleManager;
         }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = await _userManager.FindByNameAsync(model.UserName);
+            if (user == null)
+            {
+                ModelState.AddModelError(String.Empty, "Kullanıcı bulunamadı.");
+                return View(model);
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(user, model.Password, model.RememberMe, false);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError(String.Empty, "Oturum açılamadı.");
+            return View(model);
+        }
+
 
 
         public IActionResult Register()
@@ -64,6 +96,16 @@ namespace DietitianCalendarApp.Controllers
         }
 
 
+        public IActionResult LogOut()
+        {
+            _signInManager.SignOutAsync().Wait();
+            return RedirectToAction("Login");
+        }
+
+        public IActionResult Denied()
+        {
+            return View();
+        }
 
         private bool AddRole(string roleName)
         {
