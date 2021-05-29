2560ecbbd1fc6e4cde469c0f57b16e76223d9e3a
halilozat <halilozat@hotmail.com>
2021-05-29 13:05:13 +0300

    Secretary Profile Operations

---
+++ DietitianCalendarApp/DietitianCalendarApp/Controllers/ProfileController.cs
@@ -0,0 +1,60 @@
+﻿using DietitianCalendarApp.Data.Entity;
+using DietitianCalendarApp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DietitianCalendarApp.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private UserManager<AppUser> _userManager;
+
+        public ProfileController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            AppUser user = _userManager.Users.SingleOrDefault(x => x.UserName == HttpContext.User.Identity.Name);
+
+            if (user == null)
+            {
+                return View("Error");
+            }
+
+            if (_userManager.IsInRoleAsync(user, "Secretary").Result)
+            {
+                SecretaryViewModel model = new SecretaryViewModel()
+                {
+                    User = user,
+                    Dietitians = _userManager.Users.Where(x => x.IsDietitian == true)
+                };
+                return View("Secretary", model);
+            }
+            else
+            {
+
+                return View("Dietitian");
+            }
+        }
+
+
+        public IActionResult Secretary()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Dietitian")]
+        public IActionResult Dietitian()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ DietitianCalendarApp/DietitianCalendarApp/Models/SecretaryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using DietitianCalendarApp.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DietitianCalendarApp.Models
+{
+    public class SecretaryViewModel
+    {
+        public AppUser User { get; set; }
+        public IEnumerable<AppUser> Dietitians { get; set; }
+    }
+}
